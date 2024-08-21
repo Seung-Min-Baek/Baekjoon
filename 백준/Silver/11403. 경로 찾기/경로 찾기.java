fd6a68d55fd7dc0f7c8ca2c61071b01c7832f987
Seung-Min-Baek <140074653+Seung-Min-Baek@users.noreply.github.com>
2024-08-21 21:28:37 +0900

    [Silver I] Title: 경로 찾기, Time: 312 ms, Memory: 18368 KB -BaekjoonHub

--- 백준/Silver/11403. 경로 찾기/경로 찾기.java
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.java
@@ -1,56 +1,65 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-	
-	static int n;
-	static int[][] map, result;
+	static int[][] map;
+	static int[][] res_map;
 	static boolean[] visited;
-    
-	public static void main(String[] args) throws IOException {
+	static int n;
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
+		StringTokenizer st;
+
 		n = Integer.parseInt(br.readLine());
-		
-		map = new int[n+1][n+1];
-		result = new int[n+1][n+1];
-		
-		for (int i = 1; i <= n; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			for (int j = 1; j <= n; j++) {
+
+		map = new int[n][n];
+		visited = new boolean[n];
+		for (int i = 0; i < n; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < n; j++) {
 				map[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
-		
-		for (int i = 1; i <= n; i++) {
-			visited = new boolean[n+1];
-			for (int j = 1; j <= n; j++) {
-				if(map[i][j] == 1 && !visited[j])
+
+		// res_map 그리기
+		res_map = new int[n][n];
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				visited[j] = false;
+			}
+			for (int j = 0; j < n; j++) {
+				// 1. 간선 ( 연결된 부분이 1이고, false) dfs 0->1 1이 끝.
+				if (map[i][j] == 1 && !visited[j]) {
 					dfs(i, j);
+				}
 			}
 		}
-
-		for (int i = 1; i <= n; i++) {
-			for (int j = 1; j <= n; j++) {
-				System.out.print(result[i][j]+" ");
+		
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				System.out.print(res_map[i][j] + " ");
 			}
 			System.out.println();
 		}
-		
+
 	}
-	
-	static void dfs(int r, int c) {		
-		visited[c] = true;
-		result[r][c] = 1;
 		
-		for (int i = 1; i <= n; i++) {
-			if(!visited[i] && map[c][i] == 1) {
+	static void dfs(int r, int c) {
+		// 3. (0,2) 
+		// 5. (0,0)
+		res_map[r][c] = 1;
+		visited[c] = true;
+		
+		for (int i = 0; i < n; i++) {
+			// 2. 1->2 부분, 1이 시작. 연결되었으므로 dfs(0,2)
+			// 4. 2->0 부분. 연결되었으므로 dfs(0,0)
+			if (map[c][i] == 1 && !visited[i]) {
 				dfs(r, i);
 			}
 		}
-
 	}
-
 }
