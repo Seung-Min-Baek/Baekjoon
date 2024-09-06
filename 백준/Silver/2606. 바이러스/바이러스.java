614ae5d1bb2f057577d5cf444876f17f3418bcc7
Seung-Min-Baek <140074653+Seung-Min-Baek@users.noreply.github.com>
2024-09-06 13:42:50 +0900

    [Silver III] Title: 바이러스, Time: 104 ms, Memory: 14268 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -2,65 +2,43 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-	static ArrayList<Integer>[] A;
+	static ArrayList<Integer>[] connect;
 	static boolean[] visited;
-	
-	public static void main(String[] args) throws IOException {
+	static int cnt = 0;
+	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		int com = Integer.parseInt(br.readLine());
+		int n = Integer.parseInt(br.readLine());
 
-		int compnum = Integer.parseInt(br.readLine());
-		int netnum = Integer.parseInt(br.readLine());
-
-		// 초기화
-		A = new ArrayList[compnum + 1];
-		visited = new boolean[compnum + 1];
-		for (int i = 0; i < A.length; i++) {
-			A[i] = new ArrayList<>();
+		connect = new ArrayList[com+1];
+		for (int i = 0; i < com+1; i++) {
+			connect[i] = new ArrayList<>();
 		}
-		// 인접 리스트 채우기
-		for (int i = 0; i < netnum; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			int top = Integer.parseInt(st.nextToken());
-			int bot = Integer.parseInt(st.nextToken());
-			A[top].add(bot);
-			A[bot].add(top);
+		
+		for (int i = 0; i < n; i++) {
+			st = new StringTokenizer(br.readLine());
+			int p = Integer.parseInt(st.nextToken());
+			int q = Integer.parseInt(st.nextToken());
+			
+			connect[p].add(q);
+			connect[q].add(p);
 		}
-
-		// 작은번호부터 탐색하기 위해 정렬
-		for (int i = 0; i < compnum; i++) {
-			Collections.sort(A[i]);
-
-		}
-
-		System.out.println(BFS(1)-1);
+		visited = new boolean[com+1];
+		System.out.println(dfs(1));
 	}
-
-	static int BFS(int start) {
-		Queue<Integer> queue = new LinkedList<>();
-		int count = 0;
-
-		if (visited[start])
-			return count;
-		queue.add(start);
-		visited[start] = true;
-
-		while (!queue.isEmpty()) {
-			int n = queue.poll();
-			count++;
-			for (int i : A[n]) {
-				if (visited[i] == false) {
-					queue.add(i);
-					visited[i] = true;
-				}
+	private static int dfs(int x) {
+		visited[x] = true;
+		for (int i : connect[x]) {
+			if (!visited[i]) {
+				cnt++;
+				dfs(i);
 			}
-
 		}
-		return count;
+		return cnt;
 	}
 }
