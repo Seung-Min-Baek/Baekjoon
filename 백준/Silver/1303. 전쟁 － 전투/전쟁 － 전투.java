4ddf720b4c441fb1957a86bce84b902a960da1c8
Seung-Min-Baek <140074653+Seung-Min-Baek@users.noreply.github.com>
2024-10-31 15:37:34 +0900

    [Silver I] Title: 전쟁 - 전투, Time: 164 ms, Memory: 16188 KB -BaekjoonHub

--- 백준/Silver/1303. 전쟁 － 전투/전쟁 － 전투.java
+++ 백준/Silver/1303. 전쟁 － 전투/전쟁 － 전투.java
@@ -5,7 +5,6 @@ import java.util.StringTokenizer;
 
 public class Main {
 	static char[][] war;
-	static boolean[][] visited;
 	static int n, m;
 	static int[] dr = { -1, 1, 0, 0 };
 	static int[] dc = { 0, 0, -1, 1 };
@@ -18,7 +17,6 @@ public class Main {
 		n = Integer.parseInt(st.nextToken());
 
 		war = new char[n][m];
-		visited = new boolean[n][m];
 
 		for (int i = 0; i < n; i++) {
 			String[] line = br.readLine().split("");
@@ -32,28 +30,29 @@ public class Main {
 		for (int r = 0; r < n; r++) {
 			for (int c = 0; c < m; c++) {
 
-				if (war[r][c] == 'W' && !visited[r][c]) {
+				if (war[r][c] == 'W') {
 					int cnt = dfs(r, c, 'W');
 					w_sum += Math.pow(cnt, 2);
-				} else if (war[r][c] == 'B' && !visited[r][c]) {
+				} else if (war[r][c] == 'B') {
 					int cnt = dfs(r, c, 'B');
 					b_sum += Math.pow(cnt, 2);
 				}
 
 			}
 		}
-		System.out.print(w_sum + " " + b_sum);
+		System.out.print(w_sum + " ");
+		System.out.println(b_sum);
 	}
 
 	private static int dfs(int r, int c, char d) {
 		int cnt = 1;
-		visited[r][c] = true;
+		war[r][c] = 'S';
 
 		for (int i = 0; i < 4; i++) {
 			int nr = r + dr[i];
 			int nc = c + dc[i];
 
-			if (0 <= nr && nr < n && 0 <= nc && nc < m && war[nr][nc] == d && !visited[nr][nc]) {
+			if (0 <= nr && 0 <= nc && nr < n && nc < m && war[nr][nc] == d) {
 				cnt += dfs(nr, nc, d);
 			}
 		}
